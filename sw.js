34d0152acb9bff9131ff2d905b49d46b59513386
SNelectric 2026 <snelectric2026@gmail.com>
2026-09-04 02:35:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('sn-ping-store').then((cache) => {
-      return cache.addAll(['./index.html']);
+      return cache.addAll(['./index.html', './style.css', './script.js']);
     })
   );
 });
